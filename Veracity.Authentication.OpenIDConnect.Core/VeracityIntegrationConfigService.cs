38fcbebb56641755b5d4510d48eac59d251eef62
Oystein Bjorke <Oystein.Bjorke@dnvgl.com>
2018-12-06 12:24:16 +0100

    Correct spelling error in VeracityIntegrationConfigService #1 +semver: breaking

--- Veracity.Authentication.OpenIDConnect.Core/VeracityIntegrationConfigService.cs
+++ Veracity.Authentication.OpenIDConnect.Core/VeracityIntegrationConfigService.cs
@@ -5,12 +5,14 @@ namespace Veracity.Authentication.OpenIDConnect.Core
 {
     public class VeracityIntegrationConfigService : IVeracityIntegrationConfigService
     {
+        public const string SectionKey = "VeracityIntegration";
+
         private readonly VeracityIntegrationOptions _azureAdB2COptions;
         public VeracityIntegrationConfigService(IConfiguration configuration)
         {
             _azureAdB2COptions = new VeracityIntegrationOptions();
-            configuration.GetValue<VeracityIntegrationOptions>("VeracityIntegtaion");
-            configuration.GetSection("VeracityIntegtaion").Bind(_azureAdB2COptions);
+            configuration.GetValue<VeracityIntegrationOptions>(SectionKey);
+            configuration.GetSection(SectionKey).Bind(_azureAdB2COptions);
         }
 
         public VeracityIntegrationOptions GetVeracityIntegrationConfig()
